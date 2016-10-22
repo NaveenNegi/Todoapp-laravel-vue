e4f35efba57940d4b4b56298a736543a21ff4d86
NaveenNegi <neginaveen1991@gmail.com>
2016-10-23 01:38:09 +0530

    add route for mark to uncomplete task

--- routes/api.php
+++ routes/api.php
@@ -4,3 +4,5 @@ Route::get('/task','apiController@index');
 Route::post('/addtask','apiController@store');
 Route::put('/updatetask/{edit_id}','apiController@update');
 Route::delete('/delete-task/{delete_id}','apiController@delete');
+Route::get('complete-task/{complete_id}','apiController@complete');
+Route::put('/uncomplete/{task_id}','apiController@unComplete');
