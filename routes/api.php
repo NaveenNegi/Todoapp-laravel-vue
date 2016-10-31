54538d071914a92b7ef232e4211e9fb922836252
NaveenNegi <neginaveen1991@gmail.com>
2016-10-31 23:23:18 +0530

    add route for clear all completed task

--- routes/api.php
+++ routes/api.php
@@ -6,3 +6,4 @@ Route::put('/updatetask/{edit_id}','apiController@update');
 Route::delete('/delete-task/{delete_id}','apiController@delete');
 Route::get('complete-task/{complete_id}','apiController@complete');
 Route::put('/uncomplete/{task_id}','apiController@unComplete');
+Route::get('/clearcompleted','apiController@clearAll');
